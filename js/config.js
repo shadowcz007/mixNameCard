4e1ab59ac4a7a603e31aab8ffa8441cffa5215dd
s <s@shadowdeMacBook-Pro.local>
2018-07-07 18:54:25 +0800

    bug 防止name不存在

---
+++ js/config.js
@@ -0,0 +1,13 @@
+
+const cardFrom = {
+    0: {
+        name: 'mixlab',
+        gif: '1',
+        vision: '无界社区，让每个人无限可能'
+    },
+    1: {
+        name: 'cot',
+        gif: '0',
+        vision: '基于多链式区块链的通用智能硬件平台'
+    }
+};
